1f7b02aadb89aa80451fe797937becccb0c3ee6b
julian-hzd <julianhzd@hotmail.com>
2023-11-15 21:15:00 -0500

    Transparent background on colorscheme

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -5,8 +5,21 @@ return {
 	priority = 910,
 	config = function()
 		vim.cmd("colorscheme rose-pine")
-	end
-	
-} 
+    require('rose-pine').setup({
+      disable_background = true
+    })
+
+    function ColorMyPencils(color)
+      color = color or "rose-pine"
+      vim.cmd.colorscheme(color)
+
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    end
+
+    ColorMyPencils()
+  end
+}
 
 
