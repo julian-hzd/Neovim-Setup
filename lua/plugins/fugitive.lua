8d8b65422382cf5820b5538813ddf6578f9f29ca
julian-hzd <julianhzd@hotmail.com>
2024-02-05 14:40:06 -0500

    More keymaps for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,14 +1,22 @@
 return {
   "tpope/vim-fugitive",
   lazy = false,
+
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git,
       { desc = 'Git status' })
 
     vim.keymap.set("n", "<leader>ga", function()
-        vim.cmd.Git("add .")
-      end,
-      { desc = 'Git status' })
+      vim.cmd.Git("add .")
+    end, { desc = 'Git add .' })
+
+    vim.keymap.set("n", "<leader>gc", function()
+      vim.cmd.Git("commit")
+    end, { desc = 'Git commit' })
+
+    vim.keymap.set("n", "<leader>gp", function()
+      vim.cmd.Git("push")
+    end, { desc = 'Git push' })
   end
 
 }
